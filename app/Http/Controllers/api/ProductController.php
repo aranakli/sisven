b5782ff59d1507ba4b2ee484f0ce4face5fd11b0
Zemog gomez <zemogmjg@gmail.com>
2024-05-13 19:27:29 -0500

    Configuracion completa productapi

--- app/Http/Controllers/api/ProductController.php
+++ app/Http/Controllers/api/ProductController.php
@@ -19,7 +19,9 @@ class ProductController extends Controller
         ->join('categories', 'category_id', '=', 'categories.id')
         ->select('products.*', 'categories.name')
         ->get();
-        return json_encode(['products'=>$products]);
+
+        return json_encode(['products'=>$products,'categories' => $categories]);
+
     }
 
     /**
@@ -27,7 +29,20 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        
+        $product = new Product();
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+        $product->category_id = $request->category_id;
+        $product->save();
+
+        $products = DB::table('products')
+        ->join('categories', 'category_id', '=', 'categories.id')
+        ->select('products.*', 'categories.name')
+        ->get();
+
+        return json_encode(['products'=>$products,'categories' => $categories]);
+
     }
 
     /**
@@ -35,7 +50,13 @@ class ProductController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $product= Product::find($id);
+        $categories = DB::table('categories')
+        ->orderBy('name')
+        ->get();
+
+        return json_encode(['products'=>$products,'categories' => $categories]);
+
     }
 
     /**
@@ -43,7 +64,20 @@ class ProductController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $product= Product::find($id);
+       
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+        $product->category_id = $request->category_id;
+        $product->save();
+        $products = DB::table('products')
+        ->join('categories', 'category_id', '=', 'categories.id')
+        ->select('products.*', 'categories.name')
+        ->get();
+
+        return json_encode(['products'=>$products,'categories' => $categories]);
+
     }
 
     /**
@@ -51,6 +85,13 @@ class ProductController extends Controller
      */
     public function destroy(string $id)
     {
-        //
-    }
+        $product= Product::find($id);
+        $product->delete();
+
+        $products = DB::table('products')
+            ->join('categories', 'category_id', '=', 'categories.id')
+            ->select('products.*', 'categories.name')
+            ->get();
+            return json_encode(['products'=>$products,'categories' => $categories]);
+        }
 }
